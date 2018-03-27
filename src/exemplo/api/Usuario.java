cdefba2b8864910911030330f473cfd5cd8375bc
Carlos W. Gama <carloswgama@gmail.com>
2018-03-27 14:41:48 -0300

    Aula de proxy

---
+++ src/exemplo/api/Usuario.java
@@ -0,0 +1,14 @@
+package exemplo.api;
+
+public class Usuario {
+
+	public String getDadosPublicos() {
+		return "Nome, Email, Idade";
+	}
+	
+	public String getDadosPrivados() {
+		return "CPF, RG, Endereço, Telefone";
+	}
+}
+
+
